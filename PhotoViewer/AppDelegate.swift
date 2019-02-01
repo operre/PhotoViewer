5ade345c10e4023c2c0359f36c2153ff78289b2e
Ohanna Perre <ohanna.perre@gmail.com>
2019-02-01 17:53:37 -0200

    Initial commit

---
+++ PhotoViewer/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  PhotoViewer
+//
+//  Created by Ohanna Perre on 01/02/2019.
+//  Copyright © 2019 Ohanna Perre. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = ViewController()
+        self.window?.makeKeyAndVisible()
+        return true
+    }
+}
+
