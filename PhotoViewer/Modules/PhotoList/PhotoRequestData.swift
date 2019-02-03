10d7adeddaa8fe875fd07f0de8e038244508310c
Ohanna Perre <ohanna.perre@gmail.com>
2019-02-03 19:21:56 -0200

    Create photo request data with getPhotos information

---
+++ PhotoViewer/Modules/PhotoList/PhotoRequestData.swift
@@ -0,0 +1,35 @@
+//
+//  PhotoRequestData.swift
+//  PhotoViewer
+//
+//  Created by Ohanna Perre on 03/02/2019.
+//  Copyright © 2019 Ohanna Perre. All rights reserved.
+//
+
+import Foundation
+
+enum PhotoRequestData: RequestData {
+    case getPhotos
+    
+    var method: HTTPMethod {
+        return .get
+    }
+    
+    var host: String {
+        return "https://api.flickr.com"
+    }
+    
+    var path: String {
+        return "/services/rest/"
+    }
+    
+    var queryParameters: [String : String]? {
+        var parameters: [String: String] = [:]
+        parameters["api_key"] = "f9cc014fa76b098f9e82f1c288379ea1"
+        parameters["format"] = "json"
+        parameters["nojsoncallback"] = "1"
+        parameters["method"] = "flickr.photos.getRecent"
+        
+        return parameters
+    }
+}
