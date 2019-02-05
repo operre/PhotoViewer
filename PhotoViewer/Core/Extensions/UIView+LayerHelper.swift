898b770574f8fc707b6c2afd12599ffcf55102bd
Ohanna Perre <ohanna.perre@gmail.com>
2019-02-05 13:36:14 -0200

    Add corner radius to image

---
+++ PhotoViewer/Core/Extensions/UIView+LayerHelper.swift
@@ -0,0 +1,20 @@
+//
+//  UIView+LayerHelper.swift
+//  PhotoViewer
+//
+//  Created by Ohanna Perre on 05/02/2019.
+//  Copyright © 2019 Ohanna Perre. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return self.layer.cornerRadius
+        }
+        set {
+            self.layer.cornerRadius = newValue
+        }
+    }
+}
